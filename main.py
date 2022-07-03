b63dedb10d021d7c5c9a96a6efb3f3e6984716ac
Dimar <dimarylov@mail.ru>
2022-07-03 18:02:21 +0300

    Last update. All research added. Added dataframes.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import calling_function
 import preprocessing_tools
 
 
-# calling_function.parsing_by_all_folder_after()
-# calling_function.parsing_games_by_day()
+calling_function.parsing_by_all_folder_after()
+calling_function.parsing_games_by_day()
 preprocessing_tools.df_splitter_by_games()
 preprocessing_tools.df_splitter_by_maps_all_data()
 preprocessing_tools.df_splitter_by_maps_part_data()
--- preprocessing/preprocessing_one_dict.py
+++ preprocessing/preprocessing_one_dict.py
@@ -9,7 +9,8 @@ def map_splitter_all_info(game_dict):
         elif column in ['url_maps_1', 'url_maps_2']:
             for map_top in game_dict[column]:
                 for feature_on_map in game_dict[column][map_top]:
-                    game_one_string[f'{column[-1]}_{map_top}_{feature_on_map}'] = game_dict[column][map_top][feature_on_map]
+                    game_one_string[f'{column[-1]}_{map_top}_{feature_on_map}'] = \
+                        game_dict[column][map_top][feature_on_map]
         elif column == 'selected_maps':
             result_dict['1_map'] = 'did not play'
             result_dict['2_map'] = 'did not play'
@@ -23,7 +24,7 @@ def map_splitter_all_info(game_dict):
             for i in range(len(maps)):
                 result_dict[f'{i+1}_map'] = maps[i]
         else:
-            for result_on_map in range(1,game_dict['best_of']+1):
+            for result_on_map in range(1, game_dict['best_of']+1):
                 if game_dict[column][f'1_{result_on_map}st_map'] < game_dict[column][f'2_{result_on_map}st_map']:
                     result_dict[f'result_map_{result_on_map}'] = -1
                 elif game_dict[column][f'1_{result_on_map}st_map'] > game_dict[column][f'2_{result_on_map}st_map']:
@@ -60,152 +61,11 @@ def map_splitter_part_info(game_dict):
                     else:
                         key_names[f'{new_key[0]}_All_{new_key[3]}'] = key
 
-
         new_game = {}
         for key in key_names:
-            print(key, key_names[key])
             new_game[key] = game[key_names[key]]
         results.append(new_game)
     return results
-
-
-
-
-
-def map_splitter(game_dict):
-    if game_dict['best_of'] == 3:
-        maps = []
-        for game_map in game_dict['selected_maps']:
-            if game_dict['selected_maps'][game_map] == 1:
-                maps.append(game_map)
-        first_map_dict = {'map': maps[0]}
-        second_map_dict = {'map': maps[1]}
-        third_map_dict = {'map': maps[2]}
-
-        rank1 = int(game_dict['team1_world_rank'])
-        rank2 = int(game_dict['team2_world_rank'])
-
-        first_map_dict['rank1'] = rank1
-        first_map_dict['rank2'] = rank2
-        first_map_dict['diff_rank'] = rank2 - rank1
-        # для начала супер простенько, только количество игр и доля побед
-        stat1_top, stat1_all, stat2_top, stat2_all = get_names_for_urls_maps(rank1,
-                                                                             rank2,
-                                                                             maps[0])
-        first_map_dict['stat1_times_played_top'] = game_dict['url_maps_1'][stat1_top]['Times played']
-        try:
-            first_map_dict['stat1_win_rate_top'] = game_dict['url_maps_1'][stat1_top]['wins'] / \
-                game_dict['url_maps_1'][stat1_top]['Times played']
-        except ZeroDivisionError:
-            first_map_dict['stat1_win_rate_top'] = 0
-
-        first_map_dict['stat1_times_played_all'] = game_dict['url_maps_1'][stat1_all]['Times played']
-        try:
-            first_map_dict['stat1_win_rate_all'] = game_dict['url_maps_1'][stat1_all]['wins'] / \
-                game_dict['url_maps_1'][stat1_all]['Times played']
-        except ZeroDivisionError:
-            first_map_dict['stat1_win_rate_all'] = 0
-
-        first_map_dict['stat2_times_played_top'] = game_dict['url_maps_2'][stat2_top]['Times played']
-        try:
-            first_map_dict['stat2_win_rate_top'] = game_dict['url_maps_2'][stat2_top]['wins'] / \
-                game_dict['url_maps_2'][stat2_top]['Times played']
-        except ZeroDivisionError:
-            first_map_dict['stat2_win_rate_top'] = 0
-
-        first_map_dict['stat2_times_played_all'] = game_dict['url_maps_2'][stat2_all]['Times played']
-        try:
-            first_map_dict['stat2_win_rate_all'] = game_dict['url_maps_2'][stat2_all]['wins'] / \
-                game_dict['url_maps_2'][stat2_all]['Times played']
-        except ZeroDivisionError:
-            first_map_dict['stat2_win_rate_all'] = 0
-
-        first_map_dict['result'] = 1 if game_dict['results_on_maps']['1_1st_map'] > \
-                                        game_dict['results_on_maps']['2_1st_map'] else -1
-
-        second_map_dict['rank1'] = rank1
-        second_map_dict['rank2'] = rank2
-        second_map_dict['diff_rank'] = rank2 - rank1
-
-        stat1_top, stat1_all, stat2_top, stat2_all = get_names_for_urls_maps(rank1,
-                                                                             rank2,
-                                                                             maps[1])
-
-        second_map_dict['stat1_times_played_top'] = game_dict['url_maps_1'][stat1_top]['Times played']
-        try:
-            second_map_dict['stat1_win_rate_top'] = game_dict['url_maps_1'][stat1_top]['wins'] / \
-                game_dict['url_maps_1'][stat1_top]['Times played']
-        except ZeroDivisionError:
-            second_map_dict['stat1_win_rate_top'] = 0
-
-        second_map_dict['stat1_times_played_all'] = game_dict['url_maps_1'][stat1_all]['Times played']
-        try:
-            second_map_dict['stat1_win_rate_all'] = game_dict['url_maps_1'][stat1_all]['wins'] / \
-                game_dict['url_maps_1'][stat1_all]['Times played']
-        except ZeroDivisionError:
-            second_map_dict['stat1_win_rate_all'] = 0
-
-        second_map_dict['stat2_times_played_top'] = game_dict['url_maps_2'][stat2_top]['Times played']
-        try:
-            second_map_dict['stat2_win_rate_top'] = game_dict['url_maps_2'][stat2_top]['wins'] / \
-                game_dict['url_maps_2'][stat2_top]['Times played']
-        except ZeroDivisionError:
-            second_map_dict['stat2_win_rate_top'] = 0
-
-        second_map_dict['stat2_times_played_all'] = game_dict['url_maps_2'][stat2_all]['Times played']
-        try:
-            second_map_dict['stat2_win_rate_all'] = game_dict['url_maps_2'][stat2_all]['wins'] / \
-                game_dict['url_maps_2'][stat2_all]['Times played']
-        except ZeroDivisionError:
-            second_map_dict['stat2_win_rate_all'] = 0
-
-        second_map_dict['result'] = 1 if game_dict['results_on_maps']['1_2st_map'] > game_dict['results_on_maps'][
-            '2_2st_map'] else -1
-
-        if game_dict['results_on_maps']['1_3st_map'] != 0:
-            third_map_dict['rank1'] = rank1
-            third_map_dict['rank2'] = rank2
-            third_map_dict['diff_rank'] = rank2 - rank1
-
-            stat1_top, stat1_all, stat2_top, stat2_all = get_names_for_urls_maps(rank1,
-                                                                                 rank2,
-                                                                                 maps[1])
-
-            third_map_dict['stat1_times_played_top'] = game_dict['url_maps_1'][stat1_top]['Times played']
-            try:
-                third_map_dict['stat1_win_rate_top'] = game_dict['url_maps_1'][stat1_top]['wins'] / \
-                    game_dict['url_maps_1'][stat1_top]['Times played']
-            except ZeroDivisionError:
-                third_map_dict['stat1_win_rate_top'] = 0
-
-            third_map_dict['stat1_times_played_all'] = game_dict['url_maps_1'][stat1_all]['Times played']
-            try:
-                third_map_dict['stat1_win_rate_all'] = game_dict['url_maps_1'][stat1_all]['wins'] / \
-                    game_dict['url_maps_1'][stat1_all]['Times played']
-            except ZeroDivisionError:
-                third_map_dict['stat1_win_rate_all'] = 0
-
-            third_map_dict['stat2_times_played_top'] = game_dict['url_maps_2'][stat2_top]['Times played']
-            try:
-                third_map_dict['stat2_win_rate_top'] = game_dict['url_maps_2'][stat2_top]['wins'] / \
-                    game_dict['url_maps_2'][stat2_top]['Times played']
-            except ZeroDivisionError:
-                third_map_dict['stat2_win_rate_top'] = 0
-
-            third_map_dict['stat2_times_played_all'] = game_dict['url_maps_2'][stat2_all]['Times played']
-            try:
-                third_map_dict['stat2_win_rate_all'] = game_dict['url_maps_2'][stat2_all]['wins'] / \
-                    game_dict['url_maps_2'][stat2_all]['Times played']
-            except ZeroDivisionError:
-                third_map_dict['stat2_win_rate_all'] = 0
-
-            third_map_dict['result'] = 1 if game_dict['results_on_maps']['1_3st_map'] > game_dict['results_on_maps'][
-                '2_3st_map'] else -1
-            return first_map_dict, second_map_dict, third_map_dict
-        return first_map_dict, second_map_dict
-
-    else:
-        return []
 
 
 def from_dict_to_df_string(game_dict):
